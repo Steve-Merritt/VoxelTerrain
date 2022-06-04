b4babd65749e90b855408f124ed302cea18c2aee
Steve <scm191@gmail.com>
2022-06-04 16:12:03 -0700

    Initial Commit

---
+++ Source/VoxelTerrain/VoxelTerrainGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VoxelTerrainGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VOXELTERRAIN_API AVoxelTerrainGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
